977194c4c6293c48f72b0ca0503013a8fbba54a9
Jamal Eddine <jamal.eddine.eb@gmail.com>
2024-10-21 10:08:03 +0200

    test

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const path = require('path');
 
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
+
+const corsOptions = {
+    origin: 'https://www.nileshblog.tech/',//(https://your-client-app.com)
+    optionsSuccessStatus: 200,
+  };
+ 
+app.use(cors(corsOptions));
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,6 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Route to serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/tidy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'tidy_tree','tidy_tree.html'));
 });
 
 // Start the server
